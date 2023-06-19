68d9df4ba5c0b1776fc6d2899df8ff68e4b18c65
himelhasan <himelhasan497@gmail.com>
2023-06-19 11:26:21 +0600

    first commit

--- variableMe.ts
+++ variableMe.ts
@@ -11,4 +11,15 @@ userId.toFixed(2);
 // boolean
 let isLoggedIn: boolean = false;
 isLoggedIn.valueOf();
+
+// any
+
+let hero;
+
+function getHero() {
+  return "thor";
+}
+
+hero = getHero();
+
 export {};
