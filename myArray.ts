3271560e05a2ab980c04be3a0a634e17a20d479d
himelhasan <himelhasan497@gmail.com>
2023-06-19 15:55:46 +0600

    array

---
+++ myArray.ts
@@ -0,0 +1,16 @@
+const superHeros: string[] = [];
+const heroPower: number[] = [];
+const heroNumber: Array<number> = [];
+
+superHeros.push("Spiderman");
+heroPower.push(3);
+heroNumber.push(1);
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUsers: User[] = [];
+
+allUsers.push({ name: "Spiderman", isActive: true });
