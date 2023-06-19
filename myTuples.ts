da02ffa3de3e6bc474993e5d7f850986ecc934ee
himelhasan <himelhasan497@gmail.com>
2023-06-19 16:46:53 +0600

    tuples

---
+++ myTuples.ts
@@ -0,0 +1,11 @@
+const user: string[] = ["hc"];
+let tUser: [string, number, boolean];
+
+tUser = ["himel", 26, true];
+
+type User = [number, string];
+
+const newUser: User = [112, " hasdia@adjas.com"];
+newUser[1] = "sdfjds.com";
+
+export {};
